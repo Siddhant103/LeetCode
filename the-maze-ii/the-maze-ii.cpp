e3b621fb1eaffab4ff3a2a388bde0ed549dc16dd
Siddhant Patel <siddhantpatel103@gmail.com>
2021-06-10 01:26:13 +0530

    Time: 32 ms (96.96%), Space: 20.2 MB (77.62%) - LeetHub

--- the-maze-ii/the-maze-ii.cpp
+++ the-maze-ii/the-maze-ii.cpp
@@ -2,44 +2,44 @@ class Solution {
 public:
     int shortestDistance(vector<vector<int>>& grid, vector<int>& start, vector<int>& destination) {
         int rows = grid.size();
-    if(rows==0)
-        return -1;
-    int cols = grid[0].size();
-    vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
-    queue<pair<int,int>> q;
-    q.push({start[0], start[1]});
-    vector<pair<int,int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-    dist[start[0]][start[1]] = 0;
-    while(!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for(auto it: dirs)
+        if(rows==0)
+            return -1;
+        int cols = grid[0].size();
+        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
+        queue<pair<int,int>> q;
+        q.push({start[0], start[1]});
+        vector<pair<int,int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}};
+        dist[start[0]][start[1]] = 0;
+        while(!q.empty())
         {
-            int r= x + it.first;
-            int c= y + it.second;
-            int count = 0;
-            while(r>=0 && c>=0 && r<rows && c<cols && grid[r][c] == 0)
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for(auto it: dirs)
             {
-                r+= it.first;
-                c+= it.second;
-                count+=1;
+                int r= x + it.first;
+                int c= y + it.second;
+                int count = 0;
+                while(r>=0 && c>=0 && r<rows && c<cols && grid[r][c] == 0)
+                {
+                    r+= it.first;
+                    c+= it.second;
+                    count+=1;
+                }
+
+                if(dist[x][y] + count < dist[r-it.first][c-it.second])
+                {
+                    dist[r-it.first][c-it.second] = dist[x][y] + count;
+                    q.push({r-it.first, c-it.second});
+                }
             }
-            
-            if(dist[x][y] + count < dist[r-it.first][c-it.second])
-            {
-                dist[r-it.first][c-it.second] = dist[x][y] + count;
-                q.push({r-it.first, c-it.second});
-            }
+
         }
-        
-    }
-    
-    if(dist[destination[0]][destination[1]] == INT_MAX)
-        return -1;
-    
-    return dist[destination[0]][destination[1]];
+
+        if(dist[destination[0]][destination[1]] == INT_MAX)
+            return -1;
+
+        return dist[destination[0]][destination[1]];
     }
     
 };
