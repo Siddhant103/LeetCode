7c6f0dfb1884756143eecf8a3406e0755e5df991
Siddhant Patel <siddhantpatel103@gmail.com>
2021-06-25 00:37:22 +0530

    Time: 32 ms (52.92%), Space: 71.4 MB (43.35%) - LeetHub

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -11,40 +11,43 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1==NULL){
+        if(l1 == NULL && l2 == NULL){
+            return NULL;
+        }
+        if(l1 == NULL){
             return l2;
-        } else if(l2==NULL){
+        }
+        if(l2 == NULL){
             return l1;
         }
         
-        ListNode* head = NULL;
-        ListNode* prev;
-        int sum, carry = 0;
-        while(l1!=NULL || l2!=NULL){
-            sum = (l1 ? l1->val:0) + (l2 ? l2->val:0) + carry;
-            carry = sum>=10?1:0;
-            ListNode* temp = new ListNode(sum%10);
-            cout<<"SUM = "<<sum<<" CARRY = "<<carry<<endl;
+        
+        ListNode *head = NULL, *curr;
+        int carry = 0, sum;
+        while(l1 != NULL || l2 != NULL){
+            sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ListNode* temp = new ListNode(sum);
             if(head == NULL){
                 head = temp;
-                prev = head;
+                curr = head;
             } else{
-                prev->next = temp;
-                prev = prev->next;
+                curr->next = temp;
+                curr = curr->next;
             }
-            
             if(l1)
-                l1=l1->next;
-            if(l2)
-                l2=l2->next;
+                l1 = l1->next;
+            if(l2){
+                l2 = l2->next;
+            }
         }
         
-        if(carry>0){
+        if(carry == 1){
             ListNode* temp = new ListNode(carry);
-            prev->next = temp;
-            prev=prev->next;
+            curr->next = temp;
         }
-            
+        
         return head;
     }
 };
