73d15c4343cbc8e6b7d981637ed8df140673d423
Siddhant Patel <siddhantpatel103@gmail.com>
2021-06-24 22:54:56 +0530

    Time: 104 ms (47.40%), Space: 21.2 MB (43.09%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
-        
-        if(nums.size()<3)
-            return result;
-        //unordered_map<string,int> checker;
         sort(nums.begin(), nums.end());
+        int n = nums.size();
         
-        for(int i=0;i<nums.size();i++){
-            int left = i+1;
-            int right = nums.size()-1;
-            while(left<right){
-                //cout<<nums[i]<<" "<<nums[left]<<" "<<nums[right]<<endl;
-                if(nums[i] + nums[left] + nums[right] > 0){
-                    right--;
-                } else if(nums[i] + nums[left] + nums[right]<0){
-                    left++;
-                } else{
-                    vector<int> triplet {nums[i], nums[left], nums[right]};
-                    result.push_back(triplet);
-                    while(left<right && nums[left]==triplet[1]) left++;
-                    while(left<right && nums[right]==triplet[2]) right--;
-                } 
-            }
-            while(i+1<nums.size() && nums[i+1]==nums[i])
-            i++;
+        if(n <= 2){
+            return result;
         }
         
-        
+        for(int i=0;i<n;i++){
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right){
+                if(nums[i] + nums[left] + nums[right] == 0){
+                    vector<int> triplet = {nums[i], nums[left], nums[right]};
+                    while(left < right && nums[left] == triplet[1]){
+                        left++;
+                    }
+                    result.push_back(triplet);
+                    while(left < right && nums[right] == triplet[2]){
+                        right--;
+                    }
+                } else if(nums[i] + nums[left] + nums[right] < 0){
+                    left++;
+                } else{
+                    right--;
+                }
+            }
+            while(i+1 < n && nums[i] == nums[i + 1])
+                i++;
+        }
         
         return result;
     }
