d5a40d8311246495581cb69a6d1f549affb5bb19
Siddhant Patel <siddhantpatel103@gmail.com>
2021-07-14 23:41:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.28%) - LeetHub

---
+++ long-pressed-name/long-pressed-name.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        if(name.size() > typed.size())
+            return false;
+        
+        int i = 0, j = 0;
+        
+        while(j < typed.size()){
+            if(i < name.size() && name[i] == typed[j]){
+                i++;
+            } else if(j == 0 || typed[j] != typed[j-1]){
+                return false;
+            }
+            j++;
+        }
+        
+        return i == name.size();
+    }
+};
