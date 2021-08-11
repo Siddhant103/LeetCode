44f048476f02a39c0429c0efdcb8cd0394048e56
Siddhant Patel <siddhantpatel103@gmail.com>
2021-08-12 00:09:24 +0530

    Time: 24 ms (53.56%), Space: 14.8 MB (26.62%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,42 +1,28 @@
 class Solution {
-    vector<int> parent;
-    vector<int> rank;
 public:
-    int find(int x){
-        if(x == parent[x]){
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-    int unionn(int a, int b){
-        int root_a = find(a);
-        int root_b = find(b);
-        
-        if(root_a == root_b){
-            return 0;
-        } else{
-            if(rank[root_a] > rank[root_b]){
-                rank[root_a] += rank[root_b];
-                parent[root_b] = root_a;
-            } else{
-                rank[root_b] += rank[root_a];
-                parent[root_a] = root_b;
+    void dfs(int v, vector<vector<int>>& adj, vector<int>& visited){
+        visited[v] = 1;
+        for(auto neighbour: adj[v]){
+            if(visited[neighbour] == -1){
+                dfs(neighbour, adj, visited);
             }
-            return 1;
         }
     }
     int countComponents(int n, vector<vector<int>>& edges) {
-        if(n < 2)
-            return n;
-        
-        for(int i=0;i<n;++i){
-            parent.push_back(i);
-            rank.push_back(1);
+        vector<vector<int>> adj(n, vector<int>(0));
+        for (auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
         
-        int components = n;
-        for(auto edge: edges){
-            components -= unionn(edge[0], edge[1]);
+        vector<int> visited(n, -1);
+        
+        int components = 0;
+        for(int i=0;i<n;i++){
+            if(visited[i] == -1){
+                components++;
+                dfs(i, adj, visited);
+            }
         }
         
         return components;
