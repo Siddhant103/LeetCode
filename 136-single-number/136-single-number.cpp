557593175203b060c09a82d7a2c94a12f5734fa8
Siddhant Patel <siddhantpatel103@gmail.com>
2022-02-15 10:02:41 +0530

    Time: 16 ms (91.50%), Space: 16.8 MB (99.00%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        
+        for(int num: nums){
+            xorr = xorr^num;
+        }
+        
+        return xorr;
+    }
+};
