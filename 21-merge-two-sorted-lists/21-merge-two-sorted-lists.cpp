f594afd35a120681846f16aabe1e68f6e69a018e
Siddhant Patel <siddhantpatel103@gmail.com>
2022-05-21 20:01:47 +0530

    Time: 10 ms (58.17%), Space: 14.7 MB (82.44%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,51 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
-        if(l1 == NULL && l2 == NULL){
-            return NULL;
-        }
-        if(l1 == NULL){
-            return l2;
-        }
-        if(l2 == NULL){
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {        
+        if (!list1) {
+            return list2;
         }
         
-        ListNode* head = NULL;
-        ListNode* temp = head;
-            
-        if(l1->val < l2->val){
-            head = l1;
+        if (!list2) {
+            return list1;
+        }
+        
+        ListNode *head, *temp;
+        if (list1->val < list2->val) {
+            head = list1;
             temp = head;
-            l1 = l1->next;
-        } else{
-            head = l2;
+            list1 = list1->next;
+        } else {
+            head = list2;
             temp = head;
-            l2 = l2->next;
+            list2 = list2->next;
         }
         
-        while(l1 != NULL && l2 != NULL){
-            if(l1->val < l2->val){
-                temp->next = l1;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
                 temp = temp->next;
-                l1 = l1->next;
-            } else{
-                temp->next = l2;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
                 temp = temp->next;
-                l2 = l2->next;
+                list2 = list2->next;
             }
         }
         
-        while(l1!=NULL){
-            temp->next = l1;
-            temp = temp->next;
-            l1 = l1->next;
-        }
-        while(l2!=NULL){
-            temp->next = l2;
-            temp = temp->next;
-            l2 = l2->next;
+        if (list1) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
         }
         
         return head;
