839c83adcd95b9f9107205729f91f8fa7a5ed4d4
Siddhant Patel <siddhantpatel103@gmail.com>
2022-09-03 15:53:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.89%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,15 +4,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left = 1, right = n;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        int low = 1, high = n;
+        int ans = 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
-                right = mid;
+                ans = mid;
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 };
