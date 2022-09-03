e44700f572fc7745603d16c1e9a1078f9c245058
Siddhant Patel <siddhantpatel103@gmail.com>
2022-09-03 15:23:43 +0530

    Time: 48 ms (22.29%), Space: 11.1 MB (76.94%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -32,7 +32,18 @@ public:
     }
     vector<int> preorder(Node* root) {
         vector<int> result;
-        preOrder(root, result);
+        if (root == NULL)
+            return result;
+        stack<Node*> s;
+        s.push(root);
+        
+        while (!s.empty()) {
+            Node* temp = s.top(); s.pop();
+            result.push_back(temp->val);
+            for (int i = temp->children.size() - 1;i>=0;i--) {
+                s.push(temp->children[i]);
+            }
+        }
         return result;
     }
 };
