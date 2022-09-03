dddf21f346e26c69c22162948bf5fcc106caba43
Siddhant Patel <siddhantpatel103@gmail.com>
2022-09-03 18:52:23 +0530

    Time: 46 ms (69.84%), Space: 12.2 MB (82.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
-        if(n == 0){
+        if (!n) {
             return 0;
         }
-         
-        int count = 0;
         int m = grid[0].size();
+        int count = 0;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (grid[i][j] == '1') {
                     count++;
                     sinkIsland(grid, i, j, n, m);
                 }
@@ -21,9 +20,9 @@ public:
         return count;
     }
     
-    void sinkIsland(vector<vector<char>>& grid, int i, int j, int n, int m){
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0'){
-            return;
+    void sinkIsland(vector<vector<char>> &grid, int i, int j, int n, int m) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
+            return ;
         }
         
         grid[i][j] = '0';
