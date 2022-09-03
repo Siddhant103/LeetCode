b599fa12af333e280f17fc41aca1f939fdac4c39
Siddhant Patel <siddhantpatel103@gmail.com>
2022-09-03 19:35:20 +0530

    Time: 12 ms (71.13%), Space: 14 MB (78.39%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(image[sr][sc] == newColor){
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        if (image[sr][sc] == color) {
             return image;
         }
         
-        dfs(image, sr, sc, image.size(), image[0].size(), image[sr][sc], newColor);
+        dfs(image, sr, sc, n, m, image[sr][sc], color);
         return image;
     }
     
-    void dfs(vector<vector<int>>& image, int sr, int sc, int n, int m, int color, int newColor){
-        if(sr < 0 || sc < 0 || sr >= n || sc >= m || image[sr][sc] != color){
+    void dfs(vector<vector<int>> &image, int sr, int sc, int n, int m, int color, int newColor) {
+        if (sr < 0 || sc < 0 || sr >= n || sc >= m || image[sr][sc] != color) {
             return ;
         }
         
         image[sr][sc] = newColor;
-        
         dfs(image, sr + 1, sc, n, m, color, newColor);
         dfs(image, sr - 1, sc, n, m, color, newColor);
         dfs(image, sr, sc + 1, n, m, color, newColor);
