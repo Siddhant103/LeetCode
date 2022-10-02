2d3dc9fc6c600326bb813f921b971b9d87f591f5
Siddhant Patel <siddhantpatel103@gmail.com>
2022-10-02 19:09:50 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (65.90%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool areOccurrencesEqual(string s) {
-        vector<int> c(156, 0);
-        int threshold = 0;
-        
-        for (char ch: s) {
-            c[ch]++;
-            threshold = (threshold > c[ch]) ? threshold : c[ch];
-        }
-        
+        vector<int> c(156, 0);        
+        for (char ch: s) c[ch]++;
+        int threshold = c[s[0]];
         for (int i=97;i<=122;i++) {
-            if (!c[i]) {
-                continue;
-            }
-            if (c[i] != threshold) {
-                return false;
-            }
+            if (!c[i]) continue;
+            if (c[i] != threshold) return false;
         }
-        
         return true;
     }
 };
