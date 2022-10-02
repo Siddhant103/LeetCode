c334e49bf3494cfd2b37c052dd3af1dc8c6ba659
Siddhant Patel <siddhantpatel103@gmail.com>
2022-10-02 19:05:48 +0530

    Time: 10 ms (31.87%), Space: 6.7 MB (65.90%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -5,16 +5,10 @@ public:
         int threshold = 0;
         
         for (char ch: s) {
-            cout << ch;
             c[ch]++;
-            cout << c[ch] << " ";
             threshold = (threshold > c[ch]) ? threshold : c[ch];
         }
         
-        cout << threshold << endl;
-        for (int i=0;i<=155;i++)
-            cout<<c[i] << " | ";
-
         for (int i=97;i<=122;i++) {
             if (!c[i]) {
                 continue;
