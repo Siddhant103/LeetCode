2b478ae99c5b310864f9b30d3af5a7df2172c163
Siddhant Patel <siddhantpatel103@gmail.com>
2022-10-12 11:43:12 +0530

    Time: 99 ms (11.03%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool isTriangle(vector<int>& nums, int i)
-    {
-        if((nums[i-2]+nums[i-1])>nums[i])
+    bool isTriangle(vector<int>& nums, int i) {
+        if((nums[i-2] + nums[i-1]) > nums[i])
             return true;
         return false;
     }
     int largestPerimeter(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         int maxP = 0;
-        for(int i=2;i<nums.size();i++)
+        for(int i = 2;i < nums.size();i++)
         {
             if(isTriangle(nums,i))
-                maxP = max(maxP,(nums[i-2]+nums[i-1]+nums[i]));
+                maxP = max(maxP, (nums[i-2] + nums[i-1] + nums[i]));
         }
         return maxP;
     }
