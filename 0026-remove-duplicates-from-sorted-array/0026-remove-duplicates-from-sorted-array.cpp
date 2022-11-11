90389c2dc7e8d98d2f5bc33d6e5b7353719519db
Siddhant Patel <siddhantpatel103@gmail.com>
2022-11-11 10:41:04 +0530

    Time: 28 ms (42.53%), Space: 18.4 MB (37.30%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int getLastOccurance(vector<int>& nums, int target){
         int left = 0;
-        int right = nums.size()-1;
+        int right = nums.size() - 1;
         int res = -1;
         
-        while(left <= right){
-            cout<< left << " " << right << endl;
+        while (left <= right) {
             int mid = left + (right - left)/2;
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 res = mid;
                 left = mid+1;
-            } else if(nums[mid] > target){
+            } else if (nums[mid] > target) {
                 right = mid-1;
-            } else if(nums[mid] < target){
+            } else if (nums[mid] < target) {
                 left = mid+1;
             }
         }
@@ -24,14 +23,12 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
         int index = 0, start = 0;
-        while(index<n){
-            cout<< start<< " "<< index<<endl;
+        
+        while (index<n) {
             nums[start] = nums[index];
             start++;
             int lastIndex = getLastOccurance(nums, nums[index]);
-            cout << "last " << lastIndex<<endl;
             index += (lastIndex - index + 1);
-            
         }
         
         return start;
