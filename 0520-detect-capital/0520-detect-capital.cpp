8680897d8d7e5a3b26ff954899da8e4c631f2696
Siddhant Patel <siddhantpatel103@gmail.com>
2023-01-02 10:50:24 +0530

    Time: 2 ms (56.84%), Space: 6 MB (89.31%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-    for(int i = 1; i < word.size(); i++){
-        if(isupper(word[1]) != isupper(word[i]) || 
-           islower(word[0]) && isupper(word[i])) return false;
-    }        
-    return true;
-}
+        int cap = 0;
+        for(int i=0;i<word.size();i++) {
+            if(word[i] >= 65 and word[i] <= 90) cap++;
+        }
+        if(cap == word.size() or cap == 0) 
+            return true;
+
+        if(word[0] >= 65 and word[0] <= 90 and cap ==1) 
+            return true;
+        return false;
+    }
 };
