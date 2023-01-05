99d0647c2536673ae2c5e010acf11ada672346d5
Siddhant Patel <siddhantpatel103@gmail.com>
2023-01-05 10:40:47 +0530

    Time: 773 ms (51.76%), Space: 89.8 MB (68.04%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -6,15 +6,14 @@ public:
         int n = points.size(), cnt = 1, minX = points[0][0], maxX = points[0][1];
         for(int i=1;i<n;i++)
         {
-            if(points[i][0]>minX)
-                minX=points[i][0];
-            if(points[i][1]<maxX)
-                maxX=points[i][1];
-            if(minX > maxX)
-            {
+            if(points[i][0] > minX)
+                minX = points[i][0];
+            if(points[i][1] < maxX)
+                maxX = points[i][1];
+            if(minX > maxX) {
                 cnt++;
-                minX=points[i][0];
-                maxX=points[i][1];
+                minX = points[i][0];
+                maxX = points[i][1];
             }
                 
         }
