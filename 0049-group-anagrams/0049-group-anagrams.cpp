5e9e9d29ca20ae189d10174f62b8ca9767d4ef2a
Siddhant Patel <siddhantpatel103@gmail.com>
2023-01-05 18:43:37 +0530

    Time: 216 ms (8.00%), Space: 30.8 MB (5.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    string getHash(string s){
-        vector<int> count(26, 0);
-        for(char c: s){
-            count[c - 'a']++;
-        }
-        
+    string calHash(string s) {
         string hash = "";
-        for(int i=0;i<26;i++){
-            if(count[i] != 0){
-                hash += i + 'a';
-                hash += to_string(count[i]);
-            }
+        vector<int> freq(27, 0);
+        for (char c: s) {
+            freq[c - 'a']++;
         }
+        for (int i=0;i<26;i++) {
+            hash += ('a' + i);
+            hash += to_string(freq[i]);
+        }
+        cout << hash << " ";
         return hash;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        for(string s: strs){
-            string hash = getHash(s);
-            m[hash].push_back(s);
+        
+        unordered_map<string, vector<string>> tempMap;
+        
+        for (string s: strs) {
+            string sHash = calHash(s);
+            tempMap[sHash].push_back(s);
         }
         
         vector<vector<string>> result;
-        for(auto itr=m.begin();itr!=m.end();++itr){
-            result.push_back(itr->second);
+        for (auto m: tempMap) {
+            result.push_back(m.second);
         }
         
         return result;
