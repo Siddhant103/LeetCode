13c2c3d6fd7580fc09d81f78df2ebe77ffd578d0
Siddhant Patel <siddhantpatel103@gmail.com>
2023-07-19 19:36:41 +0530

    Time: 4 ms (36.64%), Space: 9.1 MB (13.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,18 +6,12 @@ public:
 
         while (i >= 0 && j >= 0) {
             nums1[target--] = nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--];
-            cout << nums1[target + 1];
         }
         
         while (i >= 0) {
-            cout << "i = ";
-            cout << i << " ";
             nums1[target--] = nums1[i--];
         }
-        cout << endl;
         while (j >= 0) {
-            cout << "j = ";
-            cout << j << " ";
             nums1[target--] = nums2[j--];
         }
     }
