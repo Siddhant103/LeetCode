d7cf76643e928cefecec8414c342d71ff8275310
Siddhant Patel <siddhantpatel103@gmail.com>
2023-07-27 22:14:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)
+        if (n == 0) 
             return 1;
-        if(n == INT_MIN){
-            x = x * x;
-            n = n/2;
+        if (n < 0) {
+            n = abs(n);
+            x = 1 / x;
         }
         
-        if(n<0){
-            n = -n;
-            x=1/x;
+        if (n % 2 == 1) {
+            return x * myPow(x * x, (n - 1) / 2);
+        } else {
+            return myPow(x * x, n / 2);
         }
-        return (n%2==0)?myPow(x*x,n/2): x*myPow(x*x,n/2);
     }
 };
