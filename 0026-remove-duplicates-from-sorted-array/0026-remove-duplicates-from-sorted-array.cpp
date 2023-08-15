2c1b6a987bd60ce8849b4a5b2b7312d624194f06
Siddhant Patel <siddhantpatel103@gmail.com>
2023-08-16 01:18:14 +0530

    Time: 11 ms (65.33%), Space: 18.4 MB (57.22%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
-    int getLastOccurance(vector<int>& nums, int target){
-        int left = 0;
-        int right = nums.size() - 1;
-        int res = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] == target) {
-                res = mid;
-                left = mid+1;
-            } else if (nums[mid] > target) {
-                right = mid-1;
-            } else if (nums[mid] < target) {
-                left = mid+1;
+    int removeDuplicates(vector<int>& nums) {
+        int index = 0, itr;
+        for (itr = 1;itr<nums.size();itr++) {
+            if (nums[itr] != nums[index]) {
+                nums[index + 1] = nums[itr];
+                index++;
             }
         }
-        
-        return res;
-    }
-    
-    int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        int index = 0, start = 0;
-        
-        while (index<n) {
-            nums[start] = nums[index];
-            start++;
-            int lastIndex = getLastOccurance(nums, nums[index]);
-            index += (lastIndex - index + 1);
-        }
-        
-        return start;
+        return index+1;
     }
 };
