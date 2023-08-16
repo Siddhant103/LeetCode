7378324730c442c02034c0978f3b5831791ceccc
Siddhant Patel <siddhantpatel103@gmail.com>
2023-08-16 23:52:22 +0530

    Time: 59 ms (60.21%), Space: 159.8 MB (22.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,60 @@
 class Solution {
 public:
-    int expandAroundCenter(string s, int left, int right){
+    // string longestPalindrome(string s) {
         
-        while(left >= 0 && right < s.size() && s[left] == s[right]){
-            left--;
-            right++;
-        }
-        
-        return right - left - 1;
-    }
+    //     int start = 0, end = 0;
+    //     int len;
+    //     for(int i=0;i<s.size();i++){
+    //         int len1 = expandAroundCenter(s, i, i);
+    //         int len2 = expandAroundCenter(s, i, i+1);
+    //         len = max(len1, len2);
+    //         if(len>end-start){
+    //             start = i-(len-1)/2;
+    //             end = i+len/2;
+    //         }
+    //     }
+    //     string result;
+    //     for(int i=start;i<=end;i++)
+    //         result += s[i];
+    //     return result;
+    // }
     
-    string longestPalindrome(string s) {
-        int left = 0, right = 0;
+    // int expandAroundCenter(string s, int left, int right){
         
-        for(int i=0;i<s.size();i++){
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = max(len1, len2);
-            if(len > (right - left)){
-                left = i - (len - 1)/2;
-                right = i + len/2;
+    //     while(left>=0 && right<s.size() && s[left] == s[right]){
+    //         left--;
+    //         right++;
+    //     }
+        
+    //     return right-left-1;
+    // }
+
+    int expandAroundCenter(string s, int i, int j) {
+        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+            i--;
+            j++;
+        }
+        return j - i - 1;
+    }
+
+    string longestPalindrome(string s) {
+        
+        int start = 0, end = 0;
+        for (int i=0;i<s.size();i++) {
+            int s1 = expandAroundCenter(s, i, i);
+            int s2 = expandAroundCenter(s, i, i + 1);
+            int len = max(s1, s2);
+            if (len > end - start) {
+                start = i - (len-1) / 2;
+                end = i + len/2;
             }
         }
-        
-        return s.substr(left, right-left+1);
+        string result = "";
+
+        for (int i=start;i<=end;i++) {
+            result += s[i];
+        }
+
+        return result;
     }
 };
