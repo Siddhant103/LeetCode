27939fa968e6a5ecff9b6b92f34e76a5c677eb27
Siddhant Patel <siddhantpatel103@gmail.com>
2023-08-24 23:45:01 +0530

    Time: 106 ms (88.9%), Space: 24 MB (85.98%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        
-        int n = nums.size();
-        vector<vector<int>> ans;
-        
-        for(int i=0; i<n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        vector<vector<int>> result;
+        int i, j, k;
+        int K = 0; // Making the question generic, 3 numbers sum should be K
+
+        for (int k=0;k<n-1;k++) {
+            i = k + 1;
+            j = n - 1;
+            while (i < j) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < K) {
+                    // Sum is lesser so we move poiter from smaller -> bigger number
+                    i++;
+                } else if (sum > K) {
+                    // Sum is greater so we move poiter from bigger -> smaller number
+                    j--;
                 } else {
-                    vector<int> triplet = {nums[i], nums[j], nums[k]};
-                    ans.push_back(triplet);
-                    j++, k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    // Equality Codition
+                    result.push_back({arr[i], arr[j], arr[k]});
+                    i++;
+                    j--;
+                    // Skipping same number as ith index
+                    while (i < j && arr[i - 1] == arr[i]) {
+                        i++;
+                    }
+                    // Skipping same number as jth index
+                    while (i < j && arr[j + 1] == arr[j]) {
+                        j--;
+                    }
                 }
             }
+            // Skipping same number as kth index
+            while (k < n - 1 && arr[k] == arr[k + 1]) {
+                k++;
+            }
         }
-        
-        return ans;
+
+        return result;
     }
 };
