712d81ada7e9c3d75a7c36053e27ac813e9337d8
Siddhant Patel <siddhantpatel103@gmail.com>
2024-01-17 20:50:17 +0530

    Time: 4 ms (82.08%), Space: 19.3 MB (22.67%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -16,9 +16,9 @@ public:
             return 0;
         }
         
-        int l = maxDepth(root->left);
-        int r = maxDepth(root->right);
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
         
-        return 1 + max(l, r);
+        return 1 + max(lh, rh);
     }
 };
