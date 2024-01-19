0e610b62543c728c0cda16dcdbc4627c6c65d40c
Siddhant Patel <siddhantpatel103@gmail.com>
2024-01-19 20:41:20 +0530

    Time: 27 ms (79.29%), Space: 15.7 MB (36.10%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    void sink(vector<vector<char>>& arr, int i, int j, int n, int m) {
-    if (i < 0 || j < 0 || i >= n || j >= m || arr[i][j] == '0') {
-        return;
-    }
-
-    arr[i][j] = '0';
-    sink(arr, i + 1, j, n, m); //bottom
-    sink(arr, i, j + 1, n, m); //right
-    sink(arr, i, j - 1, n, m); //left
-    sink(arr, i - 1, j, n, m); //top
-    // sink(arr, i + 1, j + 1, n, m);
-    // sink(arr, i - 1, j + 1, n, m);
-    // sink(arr, i + 1, j - 1, n, m);
-    // sink(arr, i + 1, j - 1, n, m);
+    void sinkIsland(vector<vector<char>> &grid, int i, int j, int n, int m) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
+            return;
+        }
+        
+        grid[i][j] = '0';
+        sinkIsland(grid, i + 1, j, n, m);
+        sinkIsland(grid, i - 1, j, n, m);
+        sinkIsland(grid, i, j - 1, n, m);
+        sinkIsland(grid, i, j + 1, n, m);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        int n = grid.size(), m = grid[0].size();
-
+        int n = grid.size();
+        int m = grid[0].size();
+        int islands = 0;
+        
         for (int i=0;i<n;i++) {
             for (int j=0;j<m;j++) {
                 if (grid[i][j] == '1') {
-                    count++;
-                    sink(grid, i, j, n, m);
+                    islands++;
+                    sinkIsland(grid, i, j, n, m);
                 }
             }
         }
-
-        return count;
+        
+        return islands;
     }
 };
