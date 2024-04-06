fd2c7f124e37ad284d27d0a766f3a38fd3f29be4
Siddhant Patel <siddhantpatel103@gmail.com>
2024-04-06 10:13:35 +0530

    Time: 19 ms (75.33%), Space: 11.6 MB (92.16%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    string modifyString(string s, int balance){
-        cout<<"\nEntered modifyString:"<<endl;
-        for(int i=s.size()-1;i>=0 && balance!=0;i--){
-            if(s[i] == '('){
-                string newString = s.substr(0,i) + s.substr(i+1);
-                cout<<"Shrinking string at i: "<<i<<endl;
-                s = newString;
-                i++;
-                balance--;
-            }    
-        }
-        return s;
-    }
     string minRemoveToMakeValid(string s) {
-        int balance = 0;
+        stack<int> stk;
         
         for(int i=0;i<s.size();i++){
-            cout<<s.size()<<" "<<endl;
-            if(s[i] == '('){
-                balance++;
-            } else if(s[i] == ')'){
-                balance--;
-            }
-            if(balance<0){
-                cout<<"Shrinking string at i: "<<i<<endl;
-                string newString = s.substr(0,i) + s.substr(i+1);
-                s = newString;
-                balance = 0;
-                i--;
+            if(s[i]=='('){
+                stk.push(i);
+            } 
+            if(s[i]==')'){
+                if(!stk.empty()){
+                    stk.pop();
+                } else{
+                    s.erase(i--, 1);
+                }
             }
         }
-        if(balance == 0)
-            return s;
-        return modifyString(s, balance);
+        while(!stk.empty()){
+            s.erase(stk.top(),1);
+            stk.pop();
+        }
+        
+        return s;
     }
 };
