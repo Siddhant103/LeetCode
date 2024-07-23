f71d4394926e0a6bf0f26b90d7b97fdd252eb53e
Siddhant Patel <siddhantpatel103@gmail.com>
2024-07-23 21:14:57 +0530

    Time: 86 ms (57.55%) | Memory: 96.1 MB (22.78%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,12 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX, totalProfit = 0;
         for (int price: prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            }
+            minPrice = min(minPrice, price);
             totalProfit = max(totalProfit, price - minPrice);
         }
-        
         return totalProfit;
     }
 };
