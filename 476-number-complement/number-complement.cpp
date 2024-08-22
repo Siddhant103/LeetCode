aadc2e01b1b7ae59d4d21ee2488d3a809d75443b
Siddhant Patel <siddhantpatel103@gmail.com>
2024-08-22 09:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (75.25%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0, tmp=num;
+        while( tmp ) {
+            tmp = tmp >> 1;
+            mask = mask << 1;
+            mask = mask | 1;
+        }
+        return ~num & mask;
+    }
+};
