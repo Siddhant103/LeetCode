ebf6f7eb63b00eddbaf6c9762ea73d10765772b0
Siddhant Patel <siddhantpatel103@gmail.com>
2025-02-19 18:34:12 +0530

    Time: 3 ms (63.86%) | Memory: 14 MB (45.68%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string expression) {
-        vector<int> res;
-        for (int i = 0; i < expression.size(); ++i) {
-            char oper = expression[i];
-            if (oper == '+' || oper == '-' || oper == '*') {
-                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
-                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
-                for (int a : s1) {
-                    for (int b : s2) {
-                        if (oper == '+') res.push_back(a + b);
-                        else if (oper == '-') res.push_back(a - b);
-                        else if (oper == '*') res.push_back(a * b);
+    // function to get the result of the operation
+    int perform(int x, int y, char op) {
+        if(op == '+') return x + y;
+        if(op == '-') return x - y;
+        if(op == '*') return x * y;
+        return 0;
+    }
+    
+    vector<int> diffWaysToCompute(string exp) {
+        
+        vector<int> results;
+        bool isNumber = 1;
+    
+        for(int i = 0; i < exp.length(); i++) {
+            // check if current character is an operator
+            if(!isdigit(exp[i])) {
+                
+                // if current character is not a digit then
+                // exp is not purely a number
+                isNumber = 0;
+                
+                // list of first operands
+                vector<int> left = diffWaysToCompute(exp.substr(0, i));
+                
+                // list of second operands
+                vector<int> right = diffWaysToCompute(exp.substr(i + 1));
+                
+                // performing operations
+                for(int x : left) {
+                    for(int y : right) {
+                        int val = perform(x, y, exp[i]);
+                        results.push_back(val);
                     }
                 }
+                
             }
         }
-        if (res.empty()) res.push_back(stoi(expression));
-        return res;
+        
+        if(isNumber == 1) results.push_back(stoi(exp));
+        return results;
     }
 };
